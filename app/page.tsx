589f289429aa666cd1dde9ff97dc05dfcaea42f5
Mandric Sittaro <mandricsittaro@yahoo.ca>
2024-03-19 20:38:59 -0400

    Change text color to white in page component

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <p className="font-black text-2xl text-black">MANDRIC SITTARO</p>
+      <p className="font-black text-2xl text-white">MANDRIC SITTARO</p>
     </div>
   );
 }
